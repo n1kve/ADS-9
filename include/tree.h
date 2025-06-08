26946d8f5ab133343211f50e2a5c467f69f68d15
n1kve <nikita06vet@gmail.com>
2025-06-08 20:14:28 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,35 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
+
+class PMTree {
+public:
+    struct Node {
+        char val;
+        std::vector<std::shared_ptr<Node>> kids;
+        Node(char v) : val(v) {}
+    };
+    PMTree(const std::vector<char>& items);
+    std::vector<std::vector<char>> getAllPerms() const;
+    std::vector<char> getPerm1(int num) const;
+    std::vector<char> getPerm2(int num) const;
+
+private:
+    std::shared_ptr<Node> root;
+    size_t total_perms;
+    void build(std::shared_ptr<Node> node, const std::vector<char>& rest);
+    void collect(std::shared_ptr<Node> node, std::vector<char>& path, 
+                std::vector<std::vector<char>>& perms) const;
+    bool findTraversal(std::shared_ptr<Node> node, std::vector<char>& path, 
+                      int& count, int target_num, std::vector<char>& res) const;
+    bool findNavigation(std::shared_ptr<Node> node, std::vector<char>& path, 
+                       int& remaining, std::vector<char>& res) const;
+    int fact(int n) const;
+};
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
