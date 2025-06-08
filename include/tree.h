2c707adb4df17f78110ed6b2181fdecc2ef15b89
n1kve <nikita06vet@gmail.com>
2025-06-08 20:22:58 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -11,7 +11,7 @@ public:
         std::vector<std::shared_ptr<Node>> kids;
         Node(char v) : val(v) {}
     };
-    PMTree(const std::vector<char>& items);
+    PMTree(const std::vector<char>& elmnts);
     std::vector<std::vector<char>> getAllPerms() const;
     std::vector<char> getPerm1(int num) const;
     std::vector<char> getPerm2(int num) const;
@@ -20,14 +20,15 @@ private:
     std::shared_ptr<Node> root;
     size_t total_perms;
     void build(std::shared_ptr<Node> node, const std::vector<char>& rest);
-    void collect(std::shared_ptr<Node> node, std::vector<char>& path, 
-                std::vector<std::vector<char>>& perms) const;
-    bool findTraversal(std::shared_ptr<Node> node, std::vector<char>& path, 
-                      int& count, int target_num, std::vector<char>& res) const;
-    bool findNavigation(std::shared_ptr<Node> node, std::vector<char>& path, 
+    void collect(std::shared_ptr<Node> node, std::vector<char>& crrnt, 
+                std::vector<std::vector<char>>& rslt) const;
+    bool findTraversal(std::shared_ptr<Node> node, std::vector<char>& crrnt, 
+                      int& cnt, int target_num, std::vector<char>& res) const;
+    bool findNavigation(std::shared_ptr<Node> node, std::vector<char>& crrnt, 
                        int& remaining, std::vector<char>& res) const;
     int fact(int n) const;
 };
+
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
 std::vector<char> getPerm1(PMTree& tree, int num);
 std::vector<char> getPerm2(PMTree& tree, int num);
